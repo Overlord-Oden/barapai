af363bf590779e1798a47dd16b5c10ee24c27373
Allassane Diomandé <allassanedioms1@gmail.com>
2026-06-23 21:08:19 +0000

    Fix wsgi: add apps/ to sys.path

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR / 'apps'))
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 application = get_wsgi_application()
